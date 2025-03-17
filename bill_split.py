f2fd09bcbe0311d3c6c752363f16b986508fd6b5
Jacob Demory <jdemory706@gmail.com>
2025-03-17 00:31:20 -0400

    Console input

--- bill_split.py
+++ bill_split.py
@@ -3,5 +3,9 @@ print("Bill Split Calculator")
 
 bill_amount = float(input())
 tip_percent = float(input())
+num_people = int(input())
+total_price = bill_amount * (1 + (tip_percent / 100))
+amount_per_person = total_price / num_people
 
-print(bill_amount * (1 + (tip_percent / 100)))
+print(f"Total (including tip): ${total_price}")
+print(f"Each person pays: ${amount_per_person}")
